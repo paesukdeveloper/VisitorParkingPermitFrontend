1672d399b9fd54a163f5ba3a30648dd253dd8d00
paesukdeveloper <developer@paesuk.co.uk>
2025-07-08 11:33:33 +0530

    Redirect using data

--- src/app/ui/modules/visiting-details/visiting-details.component.ts
+++ src/app/ui/modules/visiting-details/visiting-details.component.ts
@@ -163,7 +163,7 @@ export class VisitingDetailsComponent {
             window.location.href = `${environment.startTransactionUrl + response.Data.transactionId}`;
         }
         else if(response.Status == 200){
-            window.location.href = response.AdditionalData;
+            window.location.href = response.Data;
         }
         else if (response.Status === 400) {
           this.toaster.error(response.Message);
